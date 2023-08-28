ed239642de90c8b542bace9b7d79d326c5910743
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2023-08-27 19:44:03 -0600

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let hamburger = document.querySelector('#hamburger');
+
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+})();
+
+//  Menu Fijo
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let main = document.querySelector('main');
+    let navbarHeight = navbar.getBoundingClientRect().height;
+    let breakpoint = main.offsetTop - navbarHeight;
+    let isFixed = false;
+
+    let windowPos;
+    function updatePos() {windowPos = window.scrollY;}
+
+    function onScroll() {
+        updatePos();
+
+        if (windowPos >= breakpoint && !isFixed) {
+            navbar.classList.remove('open');
+
+            navbar.classList.add('navbar-fixed');
+            main.style.cssText = "margin-top: " + navbarHeight + 'px;';
+
+            isFixed = true;
+        }
+        else if (windowPos < breakpoint && isFixed) {
+            navbar.classList.remove('navbar-fixed');
+            main.style.cssText = "margin-top: " + 0;
+            isFixed = false;
+        }
+    }
+
+    document.addEventListener('scroll', onScroll);
+
+})();
